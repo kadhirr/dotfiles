b0b09e1e4280a56b5e1c7c502f4f5aabd8ab4e86
Kadhirravan R <53949365+kadhirr@users.noreply.github.com>
2022-01-16 17:54:08 +0530

    Refactored lua files

--- new_lua/lua/config.lua
+++ new_lua/lua/config.lua
@@ -32,3 +32,111 @@ require('lualine').setup {
   tabline = {},
   extensions = {}
 }
+----
+-- LSP & TreeSitter Config --
+----
+
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = "maintained", -- Only use parsers that are maintained
+  highlight = { -- enable highlighting
+    enable = true, 
+  },
+  indent = {
+  enable = false, -- default is disabled anyways
+  }
+}
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- LSP Config
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+          --workspace = {
+            -- Make the server aware of Neovim runtime files
+            --library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+          --}
+        }
+      }
+    }
+  end
+  server:setup(opts)
+end)
+
+-- LSP Autocomplete nvim-cmp configs
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+         vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+ mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  { name = 'ultisnips' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+--require('lspconfig')['sumneko_lua'].setup {
+ -- cmd = {'/home/kadhir/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin/lua-language-server'},
+ -- capabilities = capabilities
+--}
+
+-- Setup emmet lspconfig
+local lspconfig = require'lspconfig'
+local lsp_configs = require'lspconfig/configs'
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+if not lspconfig.emmet_ls then
+  lsp_configs.emmet_ls = {
+    default_config = {
+      cmd = {'emmet-ls', '--stdio'};
+      filetypes = {'html', 'css', 'blade'};
+      root_dir = function(fname)
+        return vim.loop.cwd()
+      end; 
+      settings = {};
+    };
+  }
+end
